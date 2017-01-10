1299bc5c019958b56d71cbc902f9f2984841aff7
Craig Buchanan <craig.buchanan@cogniza.com>
2017-01-10 09:48:27 -0800

    adding support for Uri queries

---
+++ Public/Resolve-UriQuery.Tests.ps1
@@ -0,0 +1,96 @@
+﻿Import-Module PsRepository -Force
+
+# $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+# $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+# . "$here\$sut"
+
+Describe "Resolve-UriQuery" {
+
+    Context "Container" {
+        # arrange
+        $url = "path://InfoObjects/Root Folder"
+        $expected = "SELECT TOP 200 static,SI_CUID,SI_PARENT_CUID FROM CI_INFOOBJECTS WHERE (SI_PARENTID IN (4) AND SI_NAME='Root Folder') AND ((SI_ID>='23')) ORDER BY SI_ID"
+        It "converts a path to a container to a single query" {
+            # act
+            $actual = Resolve-UriQuery -Uri $url -Verbose
+
+            # assert
+            $actual | Should Be $expected
+        }
+    }
+
+    Context "Objects in container" {
+        # arrange
+        $url = "path://InfoObjects/Root Folder/"
+        $expected = "SELECT TOP 200 static,SI_CUID,SI_PARENT_CUID FROM CI_INFOOBJECTS WHERE (SI_PARENTID IN (23)) AND ((SI_ID>='9607')) ORDER BY SI_ID"
+
+        It "converts a path to a container's objects to a single query" {
+            # act
+            $actual = Resolve-UriQuery -Uri $url -Verbose
+
+            # assert
+            $actual | Should Be $expected
+        }
+    }
+
+    Context "Objects + Container" {
+        # arrange
+        $url = "path://InfoObjects/Root Folder+/"
+        $expected = "SELECT TOP 200 static,SI_CUID,SI_PARENT_CUID FROM CI_INFOOBJECTS WHERE ((SI_PARENTID IN (23) OR SI_ID IN (23))) AND ((SI_ID>='23')) ORDER BY SI_ID"
+
+        It "converts a path to a container and its collection into two queries" {
+            # act
+            $actual = Resolve-UriQuery -Uri $url -Verbose
+
+            # assert
+            $actual | Should Be $expected
+        }
+    }
+
+    Context "PageSize parameter is set" {
+        # arrange
+        $pageSize=1
+        $url = "path://InfoObjects/Root Folder"
+        $expected = "SELECT TOP $pageSize static,SI_CUID,SI_PARENT_CUID FROM CI_INFOOBJECTS WHERE (SI_PARENTID IN (4) AND SI_NAME='Root Folder') AND ((SI_ID>='23')) ORDER BY SI_ID"
+
+        It "Adjusts the query accordingly" {
+            # act
+            $actual = Resolve-UriQuery -Uri $url -PageSize $pageSize -Verbose
+
+            # assert
+            $actual | Should Be $expected            
+        }
+    }
+
+    Context "When the number of objects exceedsthe page size" {
+        # arrange
+        $pageSize=10
+        $url = "path://InfoObjects/**[si_kind='Folder']"
+        $expected = "SELECT TOP $pageSize static,SI_CUID,SI_PARENT_CUID FROM CI_INFOOBJECTS WHERE SI_PARENTID IN (23) ORDER BY SI_ID"
+
+        It "generates multiple queries" {
+            # act
+            $actual = Resolve-UriQuery -Uri $url -PageSize $pageSize -Verbose
+
+            # assert
+            $actual.GetType() | Should Be System.Object[]
+            $actual.Count | Should BeGreaterThan 1
+        }
+    }   
+
+    Context "Pipeline" {
+        # arrange
+        $Uris = "path://InfoObjects/Root Folder","path://InfoObjects/Root Folder/"
+
+        It "Accepts URIs via the pipeline" {
+            # act
+            $actual = $Uris | Resolve-UriQuery -Verbose
+
+            # assert
+            $actual.GetType() | Should Be System.Object[]
+            $actual.Count | Should Be 2
+            
+        }
+    }
+
+}
---
+++ Public/Resolve-UriQuery.ps1
@@ -0,0 +1,101 @@
+﻿<#
+.SYNOPSIS
+    Converts a URI-formatted query (e.g. path://InfoObjects/Root Folder) to one (or more) SQL-like queries (SELECT * FROM ci_infoobjects WHERE ...).
+
+.DESCRIPTION
+    Converts a URI-formatted query (e.g. path://InfoObjects/Root Folder) to one (or more) SQL-like queries (SELECT * FROM ci_infoobjects WHERE ...).
+
+.PARAMETER Uri
+    The Uri to be processed.
+
+.PARAMETER PageSize
+    Number of InfoObjects to be returned by any given SQL query (effects the number of queries that are generated).  Default = 200
+
+.PARAMETER Incremental
+    Not sure what this really does.
+
+.EXAMPLE
+
+    # convert Uri to SQL
+    PS> Resolve-UriQuery -Uri 'path://InfoObjects/Root Folder'
+    SELECT TOP 200 static,SI_CUID,SI_PARENT_CUID FROM CI_INFOOBJECTS WHERE (SI_PARENTID IN (4) AND SI_NAME='Root Folder') AND ((SI_ID>='23')) ORDER BY SI_ID
+
+.EXAMPLE
+
+    # convert the Uri to SQL, then query the repository to get the InfoObjects
+    PS> Resolve-UriQuery -Uri 'path://InfoObjects/Root Folder' | Get-InfoObject
+
+    COM_CLASS     : System.__ComObject
+    COM_INTERFACE : System.__ComObject
+    Value         : 123456
+    Name          : SI_ID
+    Flags         : cePropFlagNone
+    Container     : False
+    Properties    : {}
+    ...    
+
+.NOTES
+    N/A
+
+.LINK
+    http://help.sap.com/businessobject/product_guides/EvIn4/en/EvIn_4_help_querybuilder_en.pdf
+
+.FUNCTIONALITY
+    PowerShell Language
+#>
+
+function Resolve-UriQuery {
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
+        [string[]]$Uri,
+        [int]$PageSize = 200,
+        [switch]$Incremental
+    )
+
+    Begin { 
+        Write-Debug "$($MyInvocation.MyCommand.Name)::BEGIN"
+
+        if ( !$global:session )
+        {
+            Get-LogonToken | Out-Null # don't add the token to the pipeline, as this will confuse 'down stream' processing
+        }
+
+        $infoStore = [CrystalDecisions.Enterprise.InfoStore]$global:session.GetService("InfoStore")
+
+        $options = New-Object CrystalDecisions.Sdk.Uri.PagingQueryOptions
+        $options.IsIncremental = $Incremental
+        $options.pageSize = $PageSize
+
+    } # /Begin
+    Process {
+        try {
+
+            foreach ($U In $Uri) {
+
+                Write-Verbose "Uri: $U"
+
+                [CrystalDecisions.Sdk.Uri.IPageResult]$pageResult = [CrystalDecisions.Enterprise.PageFactoryFacade]::PageResult($infoStore, $U, $options)
+                [System.Collections.IEnumerator]$enumerator = $pageResult.Enumerator
+
+                while ( $enumerator.MoveNext() ) 
+                {
+                    $pagedUri = [string]$enumerator.Current
+                    Write-Debug "PagedUri: $pagedUri"
+
+                    [CrystalDecisions.Sdk.Uri.IStatelessPageInfo]$pageInfo = [CrystalDecisions.Enterprise.PageFactoryFacade]::FetchPage($infoStore, $pagedUri, $options)
+
+                    $pageInfo.PageSQL
+                    Write-Debug ("PageSQL: {0}" -F $pageInfo.PageSQL )
+
+                } # /while
+
+            } # /foreach
+
+        } # /try
+        catch { Write-Error $_.Exception.Message }
+        
+    } # /Process
+    End { Write-Debug "$($MyInvocation.MyCommand.Name)::END" }
+}
