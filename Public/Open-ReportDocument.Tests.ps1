48677eb924f1c665bb4a7d0ee7b4fbea7180bece
Craig Buchanan <craig.buchanan@cogniza.com>
2017-01-24 11:52:01 -0800

    initial commit

---
+++ Public/Open-ReportDocument.Tests.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module PsEnterprise -Force
+
+# $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+# $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+# . "$here\$sut"
+
+Describe "Open-ReportDocument" {
+
+    $ID=301658
+
+    It "Opens a report" {
+        $actual = Open-ReportDocument $ID -Verbose
+        $actual | Should Be ReportDocument
+    }
+
+}
---
+++ Public/Open-ReportDocument.ps1
@@ -0,0 +1,58 @@
+﻿<#
+.SUMMARY
+    Open a Crystal ReportAppFactory
+
+.Parameter ID
+
+.Parameter KeepOpen
+
+.EXAMPLE
+
+PS> Open-ReportDocument -ID 123456
+
+#>
+function Open-ReportDocument {
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [Alias('SI_ID')]
+        [int[]]$ID,
+        [switch]$KeepOpen
+    )
+
+    Begin {
+        Write-Debug "$($MyInvocation.MyCommand.Name)::Begin"
+
+        if ( !$global:session )
+        {
+            Get-LogonToken | Out-Null # don't add the token to the pipeline, as this will confuse 'down stream' processing
+        }
+
+        # $reportAppFactory = [CrystalDecisions.ReportAppServer.ClientDoc.ReportAppFactory]$enterpriseService.Interface
+        $enterpriseService = [CrystalDecisions.Enterprise.EnterpriseService]$global:session.GetService("RASReportFactory")
+    } # /Begin
+    Process {
+        Write-Debug "$($MyInvocation.MyCommand.Name)::Process"
+
+        foreach ( $Item In $ID) {
+            try {
+                # throws cast error
+                # $reportDocument = $reportAppFactory.OpenDocument($ID,0)
+
+                $reportDocument = $enterpriseService.Interface.OpenDocument($Item, 0)
+                $reportDocument
+            }
+            catch { Write-Error $_.Exception.Message}
+            finally {
+                if ( $reportDocument -And -Not $KeepOpen ) {
+                    Write-Debug "Closing $Item"
+                    $reportDocument.Close()
+                }                
+            }
+        } # /foreach
+
+    } # /Process
+    End {Write-Debug "$($MyInvocation.MyCommand.Name)::End"}
+    
+}
